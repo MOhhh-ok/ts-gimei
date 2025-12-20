fa1846d0f455d4a4c70b0ec84725a5f737eff6a1
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2025-12-21 08:55:46 +0900

    元データを更新

--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,18 @@
-import { gimei } from './index';
+import { gimei } from "./index";
 
 // 男女含めランダム
 const manOrWoman = gimei.person();
 
 // 男性ランダム
-const man = gimei.person({ gender: 'male' });
+const man = gimei.person({ gender: "male" });
 
 // 女性ランダム
-const woman = gimei.person({ gender: 'female' });
+const woman = gimei.person({ gender: "female" });
 
 // 住所ランダム
 const address = gimei.address();
 
-console.log(manOrWoman);
-console.log(man);
-console.log(woman);
-console.log(address);
+console.log({ manOrWoman });
+console.log({ man });
+console.log({ woman });
+console.log({ address });
